b5c2cf29276afff9fec71e76c917cd36db950541
Tharusha Hapuarachchi <tharuhapuarachchi00@gmail.com>
2025-01-25 20:53:34 +0530

    mannual weather updated

--- smart-farm-security-system/expo/components/Weather.js
+++ smart-farm-security-system/expo/components/Weather.js
@@ -28,3 +28,78 @@ export default class Weather_Code extends React.Component {
       </View>
     ),
   });
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      temperature: '',
+      rain: '',
+      windspeed: '',
+      humidity: '',
+      soilMoisture: '',
+      message: '',
+      resultTxt: '',
+      result: false,
+      showAlert: false,
+      title: '',
+    };
+  }
+
+  showAlert = () => {
+    this.setState({
+      showAlert: true
+    });
+  };
+
+  hideAlert = () => {
+    this.setState({
+      showAlert: false,
+      message: '',
+      title: ''
+    });
+  };
+
+  onInsert = async (e) => {
+      if (this.state.temperature != "") {
+        if (this.state.rain != "") {
+          if (this.state.windspeed != "") {
+            if (this.state.humidity != "") {
+              if (this.state.soilMoisture != "") {
+                this.setState({ loader: true });
+                const url = "http://" + LocalIP + ":2222/weather";
+                const data = JSON.stringify({ 
+                  temperature: this.state.temperature, 
+                  rain: this.state.rain, 
+                  windspeed: this.state.windspeed, 
+                  humidity: this.state.humidity, 
+                  soilMoisture: this.state.soilMoisture 
+                });
+                await axios.post(url, data, {
+                  headers: { "Content-Type": "application/json" }
+                }).then(async (res) => {
+                  this.setState({ loader: false, result: true ,resultTxt:"Weather Code : "+res.data.res});
+                  console.log(res.data);
+                });
+              } else {
+                this.setState({ title: "Error!", message: "Please enter Soil Moisture!" });
+                this.showAlert();
+              }
+            } else {
+              this.setState({ title: "Error!", message: "Please enter Humidity!" });
+              this.showAlert();
+            }
+          } else {
+            this.setState({ title: "Error!", message: "Please enter Windspeed!" });
+            this.showAlert();
+          }
+        } else {
+          this.setState({ title: "Error!", message: "Please enter Rain!" });
+          this.showAlert();
+        }
+      } else {
+        this.setState({ title: "Error!", message: "Please enter Temperature!" });
+        this.showAlert();
+      }
+  }
+
