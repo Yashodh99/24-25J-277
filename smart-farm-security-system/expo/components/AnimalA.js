9a15ddd93b981fa17af52db07f411bd1fdf9c88e
yash.fern99 <yash.fern99@gmail.com>
2025-01-26 20:44:22 +0530

    Initial Vibration page

---
+++ smart-farm-security-system/expo/components/AnimalA.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class AnimalA extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Animal Detection Page</Text>
+            </View>
+        );
+    }
+}
