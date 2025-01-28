2e5b01059c1661b46903a0eb53233379c03673fd
yash.fern99 <yash.fern99@gmail.com>
2025-01-28 16:56:50 +0530

    Added a constructor

--- smart-farm-security-system/expo/components/AnimalA.js
+++ smart-farm-security-system/expo/components/AnimalA.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default class AnimalA extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loader: false,
+            message: '',
+            showAlert: false,
+            title: '',
+            resultTxt: '',
+            distanceValue: '',
+            vibrationValue: '',
+        };
+    }
+
     render() {
         return (
             <View>
