32768d7fc61ce954ee6182a11eea607a08b46c30
yash.fern99 <yash.fern99@gmail.com>
2025-01-30 20:50:07 +0530

    Fetch Distance Value (No Error Handling)

--- smart-farm-security-system/expo/components/AnimalA.js
+++ smart-farm-security-system/expo/components/AnimalA.js
@@ -15,10 +15,21 @@ export default class AnimalA extends React.Component {
         };
     }
 
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            fetch("http://192.168.4.1/data")
+                .then(res => res.json())
+                .then(data => {
+                    this.setState({ distanceValue: data.distanceValue });
+                });
+        }, 2000);
+    }
+
     render() {
         return (
             <View>
                 <Text>Animal Detection Page</Text>
+                <Text>Distance: {this.state.distanceValue}</Text>
             </View>
         );
     }
