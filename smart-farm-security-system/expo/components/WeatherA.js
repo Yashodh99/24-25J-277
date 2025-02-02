b92cfeabdb8a41165b3ca07d3a87535db571e60e
Tharusha Hapuarachchi <tharuhapuarachchi00@gmail.com>
2025-02-02 20:02:03 +0530

    auto weather code updated

--- smart-farm-security-system/expo/components/WeatherA.js
+++ smart-farm-security-system/expo/components/WeatherA.js
@@ -52,3 +52,74 @@ export default class Weather_Code extends React.Component {
       loader: false,
     };
   }
+
+  componentDidMount() {
+    this.requestLocationPermission();
+    this.fetchSensorData(); // Renamed to fetchSensorData for clarity
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  requestLocationPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    console.log(status);
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+    this.setState({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+    const apiKey = 'dd622b9622486c70fdaa0f2543e09cbb';
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${apiKey}&units=metric`;
+
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      const rainValue = data.rain && (data.rain["1h"] || data.rain["3h"]) ? 
+                    (data.rain["1h"] || data.rain["3h"]) : 
+                    "0";
+      await this.setState({ humidity: data.main.humidity, temperature: data.main.temp, windspeed: data.wind.speed, rain: rainValue.toString() });
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      throw error;
+    }
+  };
+
+  fetchSensorData = async () => {
+    this.setState({ loader: true });
+    this.interval = setInterval(async () => {
+      const url = "http://" + ESP32IP + "/data";
+      try {
+        const response = await axios.get(url, {
+          headers: { "Content-Type": "application/json" },
+          timeout: 20000,
+        });
+        console.log(response.data);
+        this.setState({
+          loader: false,
+          soilMoisture: response.data.soilMoistureValue + "",
+          vibration: response.data.vibrationValue + "", 
+        });
+      } catch (error) {
+        console.error('Error fetching sensor data:', error);
+        this.setState({ loader: false });
+      }
+    }, 2000);
+  };
+
+  showAlert = () => {
+    this.setState({
+      showAlert: true,
+    });
+  };
+
+  hideAlert = () => {
+    this.setState({
+      showAlert: false,
+      message: '',
+      title: '',
+    });
+  };
+  
